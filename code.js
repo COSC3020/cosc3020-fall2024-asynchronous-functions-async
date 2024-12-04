94e4e1675966dfb327c727bc7087a76d0fcf3dd7
NolanNachbar <112431921+NolanNachbar@users.noreply.github.com>
2024-12-04 10:44:49 -0700

    Update code.js

--- code.js
+++ code.js
@@ -1,10 +1,13 @@
+const async = require('async');
+
 function numMatches(ar, key) {
   let count = 0;
-    return new Promise((resolve) => {
+  return new Promise((resolve) => {
     async.each(ar, function(i, done) {
       if (i == key) count++;
       done();
-    });
+    }, function() {
       resolve(count);
     });
+  });
 }
